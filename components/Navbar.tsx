c72e09c810c022337d80fbb7f2eb56707cc29750
oussama idahmed <idhmedo1@gmail.com>
2024-01-19 20:44:06 +0100

    NAV BAR COM COMPLETE

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,76 +1,65 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../public/images/logo.png";
+import Logo from "../public/images/logo.png";
 import Link from "next/link";
 import { PiHeart, PiUser, PiMagnifyingGlass } from "react-icons/pi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    
-      <div className="   bg-slate-500 ">
-        <ul className="flex ">
-         
-          <div className="flex m-2 h-11 "> 
-          <Image className=" mr-2" alt="logo" src={logo} />
-            <li>
-              <Link
-                href="/"
-                className="text-black text-lg font-semibold tracking-normal		"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-black  text-lg font-semibold tracking-normal		"
-                href="/shop"
-              >
-                shop
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-black  text-lg font-semibold tracking-normal		"
-                href="/about"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-black  text-lg font-semibold tracking-normal		"
-                href="/contact"
-              >
-                contact
-              </Link>
-            </li>
+    <>
+      <div className="w-full h-20 bg-white sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <Image alt="logo" src={Logo} />
+            <ul className="hidden md:flex gap-x-12 font-bold text-xl text-black text-">
+              <li>
+                <Link href="/">
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/shop">
+                  <p>shop</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <p>About</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact">
+                  <p>contacts</p>
+                </Link>
+              </li>
+            </ul>
+            <ul className="hidden md:flex gap-x-12 font-bold text-2xl text-black ">
+              <li>
+                <Link href={"/"}>
+                  <PiHeart />
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"}>
+                  <PiUser />
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"}>
+                  <PiMagnifyingGlass />
+                </Link>
+              </li>
+              <li>
+                <Link href={"/"}>
+                  <AiOutlineShoppingCart />
+                </Link>
+              </li>
+            </ul>
           </div>
-          <div className="flex m-3 ">
-            <li>
-              <Link href={"/"}>
-                <PiHeart />
-              </Link>
-            </li>
-            <li>
-              <Link href={"/"}>
-                <PiUser />
-              </Link>
-            </li>
-            <li>
-              <Link href={"/"}>
-                <PiMagnifyingGlass />
-              </Link>
-            </li>
-            <li>
-              <Link href={"/"}>
-                <AiOutlineShoppingCart />
-              </Link>
-            </li>
-          </div>
-        </ul>
+        </div>
       </div>
-    
+    </>
   );
 };
 
