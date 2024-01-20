be87f42ae02717ec02af8db5c1a46c3bd60e146a
oussama idahmed <idhmedo1@gmail.com>
2024-01-21 00:19:59 +0100

    style work

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ const page = () => {
       <div>
         <Image alt="alt" src={bgImage} className=" relative  h-screen z-0" />
         <div className=" absolute top-0   2xl:w-[643px] 2xl:h-[443px] bg-[rgb(255,243,227)] right-0 mt-[300px] :mr[70px]">
-          <div className=" p-9 flex-col place-content-center	 ">
-            <p className=" text-red-700">New Arrival</p>
-            <p>Discover Our New Collection</p>
-            <p>
+          <div className=" space-y-4 p-9 flex-col place-content-center">
+            <p className=" font-bold tracking-widest text-base text">
+              New Arrival
+            </p>
+            <p className=" font-bold text-6xl text-[#B88E2F]">
+              Discover Our <br /> New Collection
+            </p>
+            <p className=" font-medium">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
               tellus, luctus nec ullamcorper mattis.
             </p>
